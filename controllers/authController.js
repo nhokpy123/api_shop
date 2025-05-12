bcba1367196569ec37a40af0a003639fb6589eda
KyNguyen756 <ky2242004@gmail.com>
2025-05-12 19:20:57 +0700

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user._id, username: user.username, email: user.email }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
 
--- server.js
+++ server.js
@@ -30,4 +30,3 @@ mongoose.connect(process.env.MONGO_URI)
   })
   .catch((err) => console.error('MongoDB error:', err));
 
-console.log(require('crypto').randomBytes(64).toString('hex'));
